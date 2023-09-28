e61718e17322a959b5f9f26e12db5dd66869c643
William Wallace <bostawwtr@outlook.com>
2023-09-28 00:21:02 -0300

    feat: criptographie

--- src/services/CreateUserService.ts
+++ src/services/CreateUserService.ts
@@ -1,11 +1,12 @@
 import { User } from "../entities/user"
 import { AppError } from "../errors/AppError"
+import { IHashRepository } from "../interfaces/IHashRepository"
 import { IUserCreateRequest } from "../interfaces/IUserInterfaces"
 import { IUserRepository } from "../interfaces/IUserRepository"
 import { validateEmail, validatePassword } from "../utils/validate"
 
 export class CreateUserService{
-    constructor(private userRepo: IUserRepository){
+    constructor(private userRepo: IUserRepository, private hashRepo: IHashRepository){
 
     }
     async execute({ name, email, password, confirmEmail, confirmPassword}: IUserCreateRequest): Promise<void> {
@@ -16,6 +17,8 @@ export class CreateUserService{
         if(!validatePassword(password)){
             throw new AppError('Email ou senha inválidos')
         }
+
+        password = await this.hashRepo.cryptographie(password);
     
         const user = new User({name, email, password, confirmEmail, confirmPassword})
         await this.userRepo.insert(user.toJson())
