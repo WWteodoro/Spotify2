cb4e08b5171797ecb64fd1d8552fe9baa431d9c2
William Wallace <bostawwtr@outlook.com>
2023-09-28 02:41:49 -0300

    feat: criptographie

--- src/routes/controllers/CreateUserController.ts
+++ src/routes/controllers/CreateUserController.ts
@@ -1,16 +1,14 @@
 import { Request, Response } from "express"
-import { validateEmail, validatePassword } from "../../utils/validate";
-import { User } from "../../entities/user";
 import { IUserRepository } from "../../interfaces/IUserRepository";
 import { CreateUserService } from "../../services/CreateUserService";
+import { IHashRepository } from "../../interfaces/IHashRepository";
 
 export class CreateUserController {
-constructor(private userRepo: IUserRepository){}
+constructor(private userRepo: IUserRepository, private hashRepo: IHashRepository){}
 
     async handle(req: Request, res: Response): Promise<Response> {
         const { name, email, password, confirmEmail, confirmPassword} = req.body;
-
-        const createUserService = new CreateUserService(this.userRepo)
+        const createUserService = new CreateUserService(this.userRepo, this.hashRepo)
         createUserService.execute({ name, email, password, confirmEmail, confirmPassword })
         
         res.status(201).send()
