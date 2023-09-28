e65cc293fc6d96c43f8b00cf94c7f527323b0a65
ph. Gradella <111555933+unbigou@users.noreply.github.com>
2023-09-28 10:18:02 -0300

    Update index

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,9 +1,11 @@
-import express from 'express';
-import { mainRouter } from './mainRoute';
+import express from "express";
+import { mainRouter } from "./mainRoute";
+import { userRoute } from "./userRoute";
 import { musicRoute } from './musicRoutes';
 
 export const route = express.Router();
 
 route.use(express.json())
-route.use('/', mainRouter);
+route.use('/', mainRouter)
+route.use('/users', userRoute)
 route.use('/music', musicRoute);
