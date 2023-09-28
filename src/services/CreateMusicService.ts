1f554fc654da32c38efc6a4335ef07feb314a4e5
ph. Gradella <111555933+unbigou@users.noreply.github.com>
2023-09-28 10:14:48 -0300

    Update CreateMusicService

--- src/services/CreateMusicService.ts
+++ src/services/CreateMusicService.ts
@@ -1,13 +1,14 @@
-import { ICreateMusicInput, IMusicRepository } from "../interfaces/IMusicInterfaces";
-import { createUUID } from "../utils/createUUID";
 import { Music } from "../entities/Music.entity";
+import { ICreateMusicInput, ICreateMusicOutput, IMusicRepository } from "../interfaces/musicInterfaces";
 
 export class CreateMusicService {
     constructor(private musicRepo: IMusicRepository) { }
 
-    async execute(input: ICreateMusicInput): Promise<void> {
-        let music = Music.create(input.author, input.album, input.name);
+    async execute(input: ICreateMusicInput): Promise<ICreateMusicOutput> {
+        const music = Music.create(input.author, input.album, input.name);
+        let response = music.toJSON()
 
-        await this.musicRepo.create(music.toJSON());
+        await this.musicRepo.create(response);
+        return response;
     }
 }
