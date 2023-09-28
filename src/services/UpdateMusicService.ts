5d13f0b0c7ca443e61ad1d6e7b2de199221d2de0
ph. Gradella <111555933+unbigou@users.noreply.github.com>
2023-09-28 10:15:23 -0300

    Update UpdateMusicService

--- src/services/UpdateMusicService.ts
+++ src/services/UpdateMusicService.ts
@@ -1,9 +1,15 @@
-import { IMusicRepository, IUpdateMusicInput } from "../interfaces/IMusicInterfaces";
+import { IMusicRepository, IUpdateMusicInput } from "../interfaces/musicInterfaces";
+import { Music } from "../entities/Music.entity";
 
 export class UpdateMusicService {
     constructor(private musicRepo: IMusicRepository) { }
 
-    async execute(input: IUpdateMusicInput): Promise<void> {
-        await this.musicRepo.update(input);
+    async execute(input: IUpdateMusicInput): Promise<IUpdateMusicInput> {
+        let { id, author, album, name } = input;
+        let music = Music.create(author, album, name, id);
+        let response = music.toJSON()
+
+        await this.musicRepo.update(response);
+        return response;
     }
 }
